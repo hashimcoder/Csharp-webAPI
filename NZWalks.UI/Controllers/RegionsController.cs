9ee32a40b50d8889573daaa51786d3253a35a2c5
hashimcoder <hashimcoder7@gmail.com>
2026-04-01 18:39:40 +0530

    updated

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
 
 namespace NZWalks.UI.Controllers
@@ -6,31 +9,121 @@ namespace NZWalks.UI.Controllers
     public class RegionsController : Controller
     {
         private readonly IHttpClientFactory httpClientFactory;
-        public RegionsController(IHttpClientFactory  httpClientFactory)
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
         {
-          this.httpClientFactory = httpClientFactory;   
+            this.httpClientFactory = httpClientFactory;
         }
-        public  async Task<IActionResult> Index()
+
+        public async Task<IActionResult> Index()
         {
-             List<RegionDto> response = new List<RegionDto>();
+            List<RegionDto> response = new List<RegionDto>();
 
             try
             {
-                  // Get all regions from webapi
-             var client = httpClientFactory.CreateClient();
-
-                var httpResponseMessage = await client.GetAsync("http://localhost:5081/api/regions");
+                var client = httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("http://localhost:5080/api/regions");
 
                 httpResponseMessage.EnsureSuccessStatusCode();
-                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+
+                response.AddRange(await httpResponseMessage.Content
+                    .ReadFromJsonAsync<IEnumerable<RegionDto>>());
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Unable to load regions. Please ensure the API and database are running.";
+            }
+
+            return View(response);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+      
+        // POST: Regions/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRegionViewModel request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpRequestMessage = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("http://localhost:5080/api/regions"),
+                    Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+                };
+
+                var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+                
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+                    if (response != null)
+                    {
+                        return RedirectToAction("Index", "Regions");
+                    }
+                }
+                else
+                {
+                    var errorResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+                    ModelState.AddModelError("", $"Failed to create region: {errorResponse}");
+                    return View(request);
+                }
             }
             catch (Exception ex)
             {
-               ViewBag.Error = "Unable to load regions. Please ensure the API and database are running.";
+                ModelState.AddModelError("", "Unable to create region. Please ensure the API is running.");
             }
-            
-             
-            return View(response);
+
+            return View(request);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+         var client = httpClientFactory.CreateClient();
+         var response = await client.GetFromJsonAsync<RegionDto>($"http://localhost:5080/api/regions/{id}");
+         if (response is not null)
+         {
+            return View(response);
+         }
+         return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid id, RegionDto request)
+        {
+             var client = httpClientFactory.CreateClient();
+             var httpRequestMessage = new HttpRequestMessage()
+             {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"http://localhost:5080/api/regions/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+             };
+             var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+             
+             if (httpResponseMessage.IsSuccessStatusCode)
+             {
+                 var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+                 if (response is not null)
+                 {
+                     return RedirectToAction("Index", "Regions");
+                 }
+             }
+             else
+             {
+                 var errorResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+                 ModelState.AddModelError(string.Empty, errorResponse);
+             }
+
+             return View(request);
+        }
+        
     }
+
 }
--- NZWalks.UI/obj/Debug/net9.0/NZWalks.UI.AssemblyInfo.cs
+++ NZWalks.UI/obj/Debug/net9.0/NZWalks.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972ec82652e0d4d4bb4af7806bccda7aa5343335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adee495e2fd970bfb006d39fda4c9e08cba1cf79")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
